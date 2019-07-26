08eb02c47904d0c16e2e7e9c63fc18600b000c1f
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-26 20:19:59 +0200

    added js code for sorting

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,12 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const bands = ['The Black Dahlia Murder', 'Thy Art is Murder', 'Gojira', 'Slipknot', 'Oh, Sleeper', 'A Day To Remember', 'An Old Dog', 'A Skylit Drive', 'We Came As Romans', 'Aversions Crown', 'Behemoth', 'Billie Eilish', 'Arctic Monkeys', 'Hate', 'A Night In Texas', 'Oceano', 'Lost Soul'];
 
-console.log(`Hello world!`);
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an)/i, '').trim(); // regular expression - ^ stands for "starts with", words that we want to ignore are in the brackets and spreaded by "|", "/i" stands for insensitive, and finally we are replacing that with nothing "''".//
+}
 
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join(''); // we need to put join at the end to join our array into the string (or else we'll get comas in our list)//
